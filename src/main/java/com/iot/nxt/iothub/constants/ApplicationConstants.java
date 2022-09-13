7a11ac14600e0b8acf7f5bc866ccd1c892f8442e
bhaskarray45 <113244946+bhaskarray45@users.noreply.github.com>
2022-09-13 12:27:40 +0000

    Update ApplicationConstants.java

--- src/main/java/com/iot/nxt/iothub/constants/ApplicationConstants.java
+++ src/main/java/com/iot/nxt/iothub/constants/ApplicationConstants.java
@@ -3,7 +3,7 @@ package com.iot.nxt.iothub.constants;
 
 public class ApplicationConstants {
 
-    public static final String IOT_HUB_CONN_STRING = "HostName=iothubnxtdev.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=GGHkFKUYOlkyXfUhwXilm2krO5ePtE9CgjeCN94UWX0=";
+    public static final String IOT_HUB_CONN_STRING = "HostName=iothubnxtdev.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=m+AGbUtjIIhRgBhA4hQKMLrpCZc3WtcjtuMp76jetUk=";
     public static final String IOT_HUB_CONN_STRING_DEVICE = "HostName=iothubnxtdev.azure-devices.net;DeviceId=iot-hub-device-first;SharedAccessKey=ItpKa46mEtylNebkI1iehQ==";
     public static final int D2C_MESSAGE_TIMEOUT = 120;
 
